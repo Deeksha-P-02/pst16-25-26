dcd7c37c72dae6a6159a6c1f1adb86f47f2b19fc
Deeksha P <deekshaparthasarathi@gmail.com>
2026-01-27 09:25:51 +0530

    Add files via upload

---
+++ Main (2).java
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+		
+
+        int a = 10;  
+        
+        System.out.println("a = " + a);
+        
+        a += 5;
+        System.out.println("a += 5 : " + a);
+        
+        a -= 3;
+        System.out.println("a -= 3 : " + a);
+        
+        
+    }
+}
+
---
+++ Main (3).java
@@ -0,0 +1,34 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+
+    
+
+        int a = 25;
+        int b = 40;
+        int c = 30;
+        
+
+        if (a > b && a > c) {
+            System.out.println("Greatest number is: " + a);
+        } 
+        else if (b > a && b > c) {
+            System.out.println("Greatest number is: " + b);
+        } 
+        else {
+            System.out.println("Greatest number is: " + c);
+        }
+    }
+}
+
+
+
+    
